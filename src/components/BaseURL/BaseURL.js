95c4ac4af0aca066c1f82dcd860e68f18934781b
context-coding <context.coding@gmail.com>
2022-03-21 12:57:38 -0500

    update

---
+++ src/components/BaseURL/BaseURL.js
@@ -0,0 +1,7 @@
+function base( url ) {
+  let base = document.createElement('base')
+  base.href = url;
+  return base;
+}
+
+export default base;
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,11 @@
 import style from './index.css';
-import component from "../../components/component.js";
+import component from "../../components/Component.js";
+import BaseURL from "../../components/BaseURL/BaseURL.js";
+import { baseURL } from '../../config/config';
 
 console.log('¡Hola!');
 console.log('adios');
 console.log('word');
 
 document.body.append(component());
+document.head.append( BaseURL( baseURL ) );
